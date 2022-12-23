6cdf3d214e9c0dc1727ff408bbcf30ae485250e2
Leona Ajdareviq <la56031@ubt-uni.net>
2022-12-23 16:38:23 +0200

    init project

--- script.js
+++ script.js
@@ -18,18 +18,8 @@ window.onscroll = () =>{
         document.querySelector('.header').classList.remove('active');
     }
 
-    /*navbar.classList.remove('active');*/
+    
 }
-
-/*window.onload = () =>{
-    if(window.scrollY > 0){
-        document.querySelector('.header').classList.add('active');
-    }else{
-        document.querySelector('.header').classList.remove('active');
-    }
-
-   
-}*/
 
 var swiper = new Swiper(".home", {
     spaceBetween: 30,
@@ -39,3 +29,28 @@ var swiper = new Swiper(".home", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+function validoMeRegex(){
+
+    var email = document.getElementById('em1').value;
+    var password = document.getElementById('pw1').value;
+
+    var regexEMAIL = /^[a-zA-Z][a-zA-Z0-9_.]+@[a-zA-Z0-9-]+\.(com|net)$/; // email duhet te startoj me shkronje dhe ka @ dhe ends .com .net
+    var regexPASSWORD = /^[A-Z][a-zA-Z0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]{6,}$/; // min 7 karaktere, karakteri i pare me uppercase dhe te kete 1nr dhe 1specchar
+
+    if(regexEMAIL.test(email)){
+        console.log("Email eshte mire")
+    }
+    else{
+        alert("Email eshte gabim!!!")
+    }
+    if(regexPASSWORD.test(password)){
+        console.log("Password eshte mire")
+    }
+    else{
+        alert("Password eshte gabim!!!")
+    }
+
+    console.log(email);
+    console.log(password);
+}
